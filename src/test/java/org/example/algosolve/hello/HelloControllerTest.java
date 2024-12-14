8452fb5471ae0fd6828215a54a8951ac3a56293d
hunko <rhwlgns4386@naver.com>
2024-12-14 16:25:24 +0900

    test : 테스트 통과 안되던 테스트 코드 수정

--- src/test/java/org/example/algosolve/hello/HelloControllerTest.java
+++ src/test/java/org/example/algosolve/hello/HelloControllerTest.java
@@ -5,14 +5,18 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 
+import org.example.algosolve.user.token.TokenProvider;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpHeaders;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
+
+import java.time.LocalDateTime;
 
 @SpringBootTest
 class HelloControllerTest {
@@ -22,6 +26,9 @@ class HelloControllerTest {
     @Autowired
     private WebApplicationContext context;
 
+    @Autowired
+    private TokenProvider tokenProvider;
+
     @BeforeEach
     public void setup() {
         mvc = MockMvcBuilders.webAppContextSetup(context).apply(springSecurity()).build();
@@ -30,6 +37,9 @@ class HelloControllerTest {
     @WithMockUser
     @Test
     public void hello가_반환된다() throws Exception {
-        mvc.perform(get("/hello")).andExpect(status().isOk()).andExpect(content().string("hello"));
+        String token = tokenProvider.createAccessToken(LocalDateTime.now(), "test");
+        mvc.perform(get("/hello").header(HttpHeaders.AUTHORIZATION,"Bearer "+token))
+                .andExpect(status().isOk())
+                .andExpect(content().string("hello"));
     }
 }
